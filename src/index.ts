c2f6c6e6da3655204f82b52abfcfe60542d5e408
Aman Paul <amanpaul155@gmail.com>
2024-01-11 17:20:54 +0530

    Setup prisma client

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import { ApolloServer } from '@apollo/server';
 import { expressMiddleware } from '@apollo/server/express4';
+import { prismaClient } from './lib/db';
 
 async function init() {
     const app = express();
@@ -9,18 +10,49 @@ async function init() {
     app.use(express.json());
     // Create GraphQL Server
     const gqlServer = new ApolloServer({
-            typeDefs: `
+      typeDefs: `
                 type Query {
                     hello: String
                     say(name: String): String
                 }
-            `,
-            resolvers: {
-                Query: {
-                    hello: () => `Hey there, I am a graphql Server`,
-                    say: ( _ , {name}: {name:  string}) => `Hey ${name} how are you ?`
+
+                type Mutation {
+                    createUser(firstName: String!, lastName: String!, email: String!, password: String!): Boolean
                 }
+            `,
+      resolvers: {
+        Query: {
+          hello: () => `Hey there, I am a graphql Server`,
+          say: (_, { name }: { name: string }) => `Hey ${name} how are you ?`,
+        },
+        Mutation: {
+          createUser: async (
+            _,
+            {
+              firstName,
+              lastName,
+              email,
+              password,
+            }: {
+              firstName: string;
+              lastName: string;
+              email: string;
+              password: string;
             }
+          ) => {
+            await prismaClient.user.create({
+                data: {
+                    firstName,
+                    lastName,
+                    email,
+                    password,
+                    salt: 'random_salt'
+                }
+            });
+            return true;
+          },
+        },
+      },
     });
 
     await gqlServer.start();
